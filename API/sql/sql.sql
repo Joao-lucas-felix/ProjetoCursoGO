046475e047987d77f98a0ed1340993b2445fe3cf
Joao-lucas-felix <jlmacena06@gmail.com>
2024-12-30 20:22:52 -0300

    Feature: Creation of the followers table, and an script to populate the database for development

--- API/sql/sql.sql
+++ API/sql/sql.sql
@@ -28,3 +28,28 @@ CREATE TRIGGER set_updated_at
 BEFORE UPDATE ON usuarios
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
+
+
+CREATE TABLE seguidores (
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE, 
+
+    seguidor_id INT NOT NULL,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE, 
+
+    PRIMARY KEY (usuario_id, seguidor_id)
+);
+
+INSERT INTO usuarios (name, nick, email, password_hash) VALUES
+    ('Usuario 1', 'Usuario 1', 'usuario1@gmail.com', '$2a$10$/BNFEUIM0spGEH.R9tjYJOgHd2npOvLC7vdxFi5MjlBjws5/H1Yxu'),
+    ('Usuario 2', 'Usuario 2', 'usuario2@gmail.com', '$2a$10$/BNFEUIM0spGEH.R9tjYJOgHd2npOvLC7vdxFi5MjlBjws5/H1Yxu'),
+    ('Usuario 3', 'Usuario 3', 'usuario3@gmail.com', '$2a$10$/BNFEUIM0spGEH.R9tjYJOgHd2npOvLC7vdxFi5MjlBjws5/H1Yxu');
+
+INSERT INTO seguidores (usuario_id, seguidor_id) VALUES 
+    (1,2),
+    (1,3),
+    (3,1);
