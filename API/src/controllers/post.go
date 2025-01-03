b549d578eba6ce7cc04be5979264663ff11f1440
Joao-lucas-felix <jlmacena06@gmail.com>
2025-01-03 16:29:56 -0300

    Feature: creating the post routes and the controller wihtout implementations, and  adding the routes to the ConfigRoutes

---
+++ API/src/controllers/post.go
@@ -0,0 +1,26 @@
+package controllers
+
+import "net/http"
+
+// CreatePost is the endpoint to create a new post
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+// GetAllPost is the endpoint to get posts
+func GetAllPosts (w http.ResponseWriter, r *http.Request) {
+
+}
+// GetPostById is the enpoint to get a  post by id
+func GetPostById(w http.ResponseWriter, r *http.Request) {
+
+}
+// UpdatePost is the endpoint to update a post
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+// DeletePost is the endpoint to delete a post
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
